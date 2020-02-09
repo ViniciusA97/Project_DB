eb99ee2dd961defebef53db6be840c5aaec5d6de
ViniciusA97 <vinicius.queiroz@dce.ufpb.br>
2020-02-09 11:27:00 -0300

    finalização do Animed Widget... To do: Parte lógica

--- project_bd/lib/pages/HomeRestPages/InitRest.dart
+++ project_bd/lib/pages/HomeRestPages/InitRest.dart
@@ -25,6 +25,15 @@ class _InitRestState extends State<InitRest> {
   String name;
   String img;
   String search;
+  Widget _search = Container(
+    width: 0,
+    height: 0,
+  );
+  Widget _create = Container(
+    width: 0,
+    height: 0,
+  );
+  Widget tab =Container(width: 0,height: 0,);
 
   @override
   void initState() {
@@ -136,55 +145,22 @@ class _InitRestState extends State<InitRest> {
         Stack(children: <Widget>[
           Positioned(
               child: AnimatedContainer(
-            alignment: alignment,
-            duration: Duration(seconds: 1),
-            height: x,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)),
-                color: Colors.grey.shade100),
-            curve: Curves.fastLinearToSlowEaseIn,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Text('Categorias'),
-                DefaultTabController(
-                  length: 2,
-                  initialIndex: 0,
-                  child: Column(
-                    children: <Widget>[
-                      TabBar(
-                        isScrollable: true,
-                        labelColor: Colors.white,
-                        tabs: <Widget>[
-                          Tab(
-                            child: Text(
-                              'Create',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          ),
-                          Tab(
-                              child: Text('Search',
-                                  style: TextStyle(color: Colors.black)))
-                        ],
-                      ),
-                      Container(
-                        height: 200,
-                        width: MediaQuery.of(context).size.width,
-                        child: TabBarView(
-                          children: <Widget>[getCreate(), getSerch()],
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ))
+                  alignment: alignment,
+                  onEnd: (){
+                    _setCreate();
+                    _setSerch();
+                    _getTabView();
+                  },
+                  duration: Duration(seconds: 1),
+                  height: x,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                      color: Colors.grey.shade100),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  child: tab))
         ]),
       ])
     ]);
@@ -215,6 +191,7 @@ class _InitRestState extends State<InitRest> {
         ],
       );
     } else {
+      setState(() {});
       return ListView.builder(
           itemCount: this._categories.length,
           itemBuilder: (BuildContext cntx, int index) {
@@ -232,62 +209,81 @@ class _InitRestState extends State<InitRest> {
     }
   }
 
-  Widget getCreate() {
-    return Container(
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        child: Form(
-          key: key,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    hintText: 'Nome da categoria',
-                    fillColor: Colors.white,
-                    filled: false),
-                onSaved: (val) => name = val,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 3),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    hintText: 'Imagem da categoria',
-                    fillColor: Colors.white,
-                    filled: false),
-                onSaved: (val) => img = val,
-              ),
-              Padding(padding: EdgeInsets.only(top: 10)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                      child: Text('Create'),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                      onPressed: () {
-                        _save();
-                      }),
-                      Padding(padding: EdgeInsets.fromLTRB(20, 5, 0, 0)),
-                  RaisedButton(
-                    child: Text('Cancel'),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                    onPressed: (){setState(() {
-                      x=0;
-                  });})
-                ],
-              )
-            ],
-          ),
-        ));
+   _setCreate() {
+    setState(() {
+      this._create = Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Form(
+            key: key,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      hintText: 'Nome da categoria',
+                      fillColor: Colors.white,
+                      filled: false),
+                  onSaved: (val) => name = val,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 3),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      hintText: 'Imagem da categoria',
+                      fillColor: Colors.white,
+                      filled: false),
+                  onSaved: (val) => img = val,
+                ),
+                Padding(padding: EdgeInsets.only(top: 10)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                        child: Text('Create'),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: () {
+                          _save();
+                          setState(() {
+                            x=0;
+                            this._search= Container(height: 0,width: 0,);
+                            this._create= Container(height: 0,width: 0,);
+                            tab = Container(height: 0,width: 0,);
+
+                          });
+                        }),
+                    Padding(padding: EdgeInsets.fromLTRB(20, 5, 0, 0)),
+                    RaisedButton(
+                        child: Text('Cancel'),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        onPressed: () {
+                          setState(() {
+                            x=0;
+                            this._search= Container(height: 0,width: 0,);
+                            this._create= Container(height: 0,width: 0,);
+                            tab = Container(height: 0,width: 0,);
+                            
+                          });
+                        })
+                  ],
+                )
+              ],
+            ),
+          ));
+    });
   }
 
-  Widget getSerch() {
-    return Container(
-      width: MediaQuery.of(context).size.width - 20,
-    );
+  void _setSerch() {
+    setState(() {
+      this._search = Container(
+        width: MediaQuery.of(context).size.width - 20,
+      );
+    });
   }
 
   void _save() async {
@@ -306,5 +302,48 @@ class _InitRestState extends State<InitRest> {
         print('foi');
       });
     }
+  }
+
+  _getTabView() {
+    setState(() {
+      tab = Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+          ),
+          Text('Categorias'),
+          DefaultTabController(
+            length: 2,
+            initialIndex: 0,
+            child: Column(
+              children: <Widget>[
+                TabBar(
+                  isScrollable: true,
+                  labelColor: Colors.white,
+                  tabs: <Widget>[
+                    Tab(
+                      child: Text(
+                        'Create',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    Tab(
+                        child: Text('Search',
+                            style: TextStyle(color: Colors.black)))
+                  ],
+                ),
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  child: TabBarView(
+                    children: <Widget>[this._create, this._search],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      );
+    });
   }
 }
