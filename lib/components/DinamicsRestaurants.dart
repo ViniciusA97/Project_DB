abf81245f7ad8f842cd0fe1853728fe9a3ab5ab7
joaopsilvam <joaopsilvam88@gmail.com>
2020-03-25 01:35:26 -0300

    Add files via upload
    Sacola implementada.

---
+++ lib/components/DinamicsRestaurants.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project_bd/Control/Control.dart';
+import 'package:project_bd/Model/restaurant.dart';
+import 'package:project_bd/pages/HomeUserPage/restaurantPage.dart';
+
+import '../constants.dart';
+
+class DinamicRestaurants extends StatefulWidget {
+  String _name;
+
+  DinamicRestaurants(this._name);
+
+  @override
+  State<StatefulWidget> createState() => _DinamicRestaurantsState(this._name);
+}
+
+class _DinamicRestaurantsState extends State<DinamicRestaurants> {
+  String _name;
+
+  List<Restaurant> _rest = new List<Restaurant>();
+  _DinamicRestaurantsState(this._name);
+
+  @override
+  void initState() {
+    setState(() {
+      Control _control = Control.internal();
+      Map<String, Future<List<Restaurant>>> map = {
+        "Restaurante popular": _control.getRestPopular(),
+        "Promoção": _control.getRestPromocao(),
+        "Entrega gratis": _control.getRestEntregaGratis(),
+        "Entrega rápida": _control.getRestEntregaRapida(),
+        "Mais pedidos": _control.getRestMaisPedido()
+      };
+      map[_name].then((onValue) {
+        setState(() {
+          _rest = onValue;
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getContainer(),
+    );
+  }
+
+  Widget getContainer() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            height: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                FlatButton(
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Color(0xff38ad53),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width / 15),
+                ),
+                Text(
+                  '${this._name}'.toUpperCase(),
+                  style: TextStyle(fontSize: 16, letterSpacing: 1.0),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+          ),
+          Container(
+            height: 10,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.grey.shade200,
+          ),
+          Padding(padding: EdgeInsets.only(top: 15)),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.75,
+            width: MediaQuery.of(context).size.width - 20,
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ListView.builder(
+              itemCount: this._rest.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (this._rest.isEmpty) {
+                  return Center(
+                    child: Text(
+                        "Ainda não foi cadastrado nenhum restaurante popular"),
+                  );
+                } else {
+                  return MaterialButton(
+                    onPressed: () {
+                      _call(this._rest[index]);
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 20,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade200,
+                            spreadRadius: 1.0, //ext
+                          )
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            height: 80,
+                            width: 130,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: const Radius.circular(20),
+                              ),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: const Radius.circular(10),
+                                  bottomLeft: const Radius.circular(10)),
+                              child: Image.network(
+                                '${this._rest[index].url}',
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 130,
+                            margin: EdgeInsets.fromLTRB(15, 0, 10, 0),
+                            child: Center(
+                              child: Text('${this._rest[index].name}',
+                                  style: kTextRest),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _call(Restaurant t) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => RestaurantPage(t)));
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const kTextFieldDecoraction = InputDecoration(
+  hintStyle: TextStyle(color: Colors.grey),
+  // fillColor: Color.fromRGBO(144, 238, 144, .1),
+  filled: true,
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromRGBO(118, 192, 68, .1), width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromRGBO(118, 192, 68, .1), width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  ),
+);
+
+const kTextTitle = TextStyle(
+  letterSpacing: 2.5,
+  color: Colors.black54,
+  fontWeight: FontWeight.bold,
+);
+
+const kTextCategorie = TextStyle(
+  fontSize: 12, 
+  letterSpacing: 0.5, 
+  color: Colors.black54,
+);
+
+const kTextRest = TextStyle(
+  fontSize: 18, 
+  letterSpacing: 0.5, 
+  color: Colors.black54,
+);
